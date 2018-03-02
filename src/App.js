71107a9f16d72122b9800b8a422c11814b19372e
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-03-02 13:43:28 +0545

    fetched mates data from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBar from './SearchBar';
-import {mates} from "./db"
 
 
 class App extends Component{
     constructor(){
         super();
         this.state={
-            mates: mates,
+            mates: [],
             searchField: ''
         }
+    }
+    componentDidMount(){
+        fetch('https://api.myjson.com/bins/799p1')
+        .then((res)=>{
+            return res.json()
+        })
+        .then(infos=> this.setState({mates: infos}))
     }
    
     handleSearch = (event)=>{
         this.setState({searchField: event.target.value})
     }
+   
     render(){
         const filteredMates = this.state.mates.filter((mate)=>{
             return mate.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         })
+        if(this.state.mates.length === 0) {return <h1>Loading</h1>}
         return(
             <React.Fragment>
                 <h2 className="tc">MBM 2072</h2>
