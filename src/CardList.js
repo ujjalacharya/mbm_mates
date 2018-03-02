c0bf7a64cfccf8e9296ae2754e2f40f53eeaf7a4
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-03-02 12:56:34 +0545

    made cleaner

--- src/CardList.js
+++ src/CardList.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import Card from "./Card";
 
-class CardList extends Component{
-    render(){
-        const {mates} = this.props;
-        const cardlist = mates.map((mate, i)=>{
-            return <Card name={mates[i].name} img={mates[i].img} key={mates[i].id}/>
-        }) 
-        return(
-            <div>
-            {cardlist}                
-            </div>
+class CardList extends Component {
+    render() {
+        const { mates } = this.props;
+        return (
+            <div> {
+                mates.map((mate, i) => {
+                    return (<Card name={mates[i].name} img={mates[i].img} key={mates[i].id} />)
+                })
+            }</div>
         )
     }
 }
