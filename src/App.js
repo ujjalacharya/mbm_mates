cb4e58ae86d41118474f76a3cf047b88c99216af
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-03-02 13:49:20 +0545

    made scrollable

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBar from './SearchBar';
+import Scroll from './Scroll'
 
 
-class App extends Component{
-    constructor(){
+class App extends Component {
+    constructor() {
         super();
-        this.state={
+        this.state = {
             mates: [],
             searchField: ''
         }
     }
-    componentDidMount(){
+    componentDidMount() {
         fetch('https://api.myjson.com/bins/799p1')
-        .then((res)=>{
-            return res.json()
-        })
-        .then(infos=> this.setState({mates: infos}))
+            .then((res) => {
+                return res.json()
+            })
+            .then(infos => this.setState({ mates: infos }))
     }
-   
-    handleSearch = (event)=>{
-        this.setState({searchField: event.target.value})
+
+    handleSearch = (event) => {
+        this.setState({ searchField: event.target.value })
     }
-   
-    render(){
-        const filteredMates = this.state.mates.filter((mate)=>{
+
+    render() {
+        const filteredMates = this.state.mates.filter((mate) => {
             return mate.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         })
-        if(this.state.mates.length === 0) {return <h1>Loading</h1>}
-        return(
+        if (this.state.mates.length === 0) { return <h1>Loading</h1> }
+        return (
             <React.Fragment>
                 <h2 className="tc">MBM 2072</h2>
-                <SearchBar searchChange={this.handleSearch}/>
-            <CardList mates={filteredMates}/>
+                <SearchBar searchChange={this.handleSearch} />
+                <Scroll>
+                <CardList mates={filteredMates} />                    
+                </Scroll>
             </React.Fragment>
         )
     }
